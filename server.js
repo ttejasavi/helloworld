576833e600f78ad48f10705ca97de3554e1f3eb9
ttejasavi <133807850+ttejasavi@users.noreply.github.com>
2023-05-18 13:28:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Tejasavi - 174401216. My favourite colour is blue and I love to enjoy cherry blossom.");
 });
 
 // setup http server to listen on HTTP_PORT
